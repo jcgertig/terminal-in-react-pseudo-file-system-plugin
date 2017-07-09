af7c50c5d8b572df3980c5471f568053fb5f99fa
Jonathan Gertig <jcgertig@gmail.com>
2017-07-09 05:36:40 -0500

    fix remove and path parse

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ function has(obj, key) {
 @autobind
 export default class PseudoFileSystem {
   name = 'PseudoFileSystem';
-  version = '1.0.0';
+  version = '1.0.19';
 
   constructor(pathSeporator = '/') {
     this.pathSeporator = pathSeporator;
@@ -44,9 +44,10 @@ export default class PseudoFileSystem {
       },
     };
 
+    window.filesystem = this.filesystem;
 
     const _ = [
-      this.pathSeporator + HOME_PATH.join(this.pathSeporator) + this.pathSeporator,
+      `${this.pathSeporator}${HOME_PATH.join(this.pathSeporator)}${this.pathSeporator}`,
     ];
     this.enterDir().method({ _ });
   }
@@ -68,24 +69,29 @@ export default class PseudoFileSystem {
   };
 
   @decorate(memoize(500))
-  doParse(split) {
+  doParse(split, currentPath) { // eslint-disable-line class-methods-use-this
     let isDir = false;
     let isRoot = false;
-    if (split[split.length - 1] === '') {
+    const baseIsASymbol = [CURRENT_DIR, PARENT_DIR, HOME_DIR].indexOf(split[0]) > -1;
+    if (split[split.length - 1] === '' || (split.length === 1 && baseIsASymbol)) {
       isDir = true;
     }
     if (split[0] === '') {
       isRoot = true;
     }
-    let modPath = split.filter(part => part.length === 0);
+    let modPath = split.filter(part => part.length !== 0);
     if (!isRoot) {
       if (modPath[0] === CURRENT_DIR) {
-        modPath = [...this.currentPath, ...modPath.slice(1)];
+        modPath = [...currentPath.parts, ...modPath.slice(1)];
       } else if (modPath[0] === HOME_DIR) {
         modPath = [...HOME_PATH, ...modPath.slice(1)];
       } else if (modPath[0] === PARENT_DIR) {
-        modPath = [...this.currentPath, ...modPath];
+        modPath = [...currentPath.parts, ...modPath];
       }
+    }
+
+    if (baseIsASymbol) {
+      isRoot = true;
     }
 
     for (let i = 0; i < modPath.length; i += 1) {
@@ -98,13 +104,21 @@ export default class PseudoFileSystem {
         modPath[i] = '';
       }
     }
-    modPath = modPath.filter(part => part.length === 0);
+    modPath = modPath.filter(part => part.length !== 0);
 
     return {
       parts: modPath,
       isRoot,
       isDir,
     };
+  }
+
+  parsePath(path) {
+    const split = path.split(this.pathSeporator);
+    if (['', CURRENT_DIR, PARENT_DIR, HOME_DIR].indexOf(split[0]) < 0) {
+      split.unshift('.');
+    }
+    return this.doParse(split, this.currentPath);
   }
 
   toStringPath(path) {
@@ -116,11 +130,6 @@ export default class PseudoFileSystem {
       stringParts.unshift('');
     }
     return stringParts.join(this.pathSeporator);
-  }
-
-  parsePath(path) {
-    const split = path.split(this.pathSeporator);
-    return this.doParse(split);
   }
 
   isValidPath(path) {
@@ -151,16 +160,19 @@ export default class PseudoFileSystem {
   }
 
   addToFileSystem({ parts }, data) {
-    set(this.filesystem, `contents.${parts.join('.contents.')}`, data);
+    const path = ['contents', ...parts.join('Ω…ΩcontentsΩ…Ω').split('Ω…Ω')];
+    set(this.filesystem, path, data);
   }
 
   enterDir() {
     return {
       method: (args) => {
-        const newPath = this.parsePath(args._.join(' '));
-        if (this.isValidPath(newPath)) {
-          this.currentPath = newPath;
-          this.api.setPromptPrefix(`${this.toStringPath(this.currentPath)} `);
+        if (args._.length > 0) {
+          const newPath = this.parsePath(args._.join(' '));
+          if (this.isValidPath(newPath)) {
+            this.currentPath = newPath;
+            this.api.setPromptPrefix(`${this.toStringPath(this.currentPath)} `);
+          }
         }
       },
     };
@@ -169,17 +181,19 @@ export default class PseudoFileSystem {
   createDir() {
     return {
       method: (args) => {
-        const path = this.parsePath(args._[0]);
-        const parentDir = path.parts.slice(0, path.parts.length - 2);
-        const newDir = path.parts[path.parts.length - 1];
-        const dir = this.getContents({ parts: parentDir });
-        if (dir !== null) {
-          if (!has(dir.contents, newDir)) {
-            this.addToFileSystem(path, {
-              name: newDir,
-              type: DIR,
-              contents: {},
-            });
+        if (args._.length > 0) {
+          const path = this.parsePath(args._[0]);
+          const parentDir = path.parts.slice(0, path.parts.length - 2);
+          const newDir = path.parts[path.parts.length - 1];
+          const dir = this.getContents({ parts: parentDir });
+          if (dir !== null) {
+            if (!has(dir.contents, newDir)) {
+              this.addToFileSystem(path, {
+                name: newDir,
+                type: DIR,
+                contents: {},
+              });
+            }
           }
         }
       },
@@ -189,17 +203,19 @@ export default class PseudoFileSystem {
   createFile() {
     return {
       method: (args) => {
-        const path = this.parsePath(args._[0]);
-        const parentDir = path.parts.slice(0, path.parts.length - 2);
-        const newFile = path.parts[path.parts.length - 1];
-        const dir = this.getContents({ parts: parentDir });
-        if (dir !== null) {
-          if (!has(dir.contents, newFile)) {
-            this.addToFileSystem(path, {
-              name: newFile,
-              type: FILE,
-              contents: '',
-            });
+        if (args._.length > 0) {
+          const path = this.parsePath(args._[0]);
+          const parentDir = path.parts.slice(0, path.parts.length - 2);
+          const newFile = path.parts[path.parts.length - 1];
+          const dir = this.getContents({ parts: parentDir });
+          if (dir !== null) {
+            if (!has(dir.contents, newFile)) {
+              this.addToFileSystem(path, {
+                name: newFile,
+                type: FILE,
+                contents: '',
+              });
+            }
           }
         }
       },
@@ -209,17 +225,19 @@ export default class PseudoFileSystem {
   removeFromFileSystem() {
     return {
       method: (args) => {
-        const path = this.parsePath(args._[0]);
-        const contents = this.getContents(path);
-        if (contents !== null) {
-          if (
-            contents.type === DIR &&
-            Object.keys(contents.contents).length > 0 &&
-            !args.recursive
-          ) {
-            this.api.printLine(`${this.toStringPath(path)} is not empty`);
-          } else {
-            this.addToFileSystem(path, undefined);
+        if (args._.length > 0) {
+          const path = this.parsePath(args._[0]);
+          const contents = this.getContents(path);
+          if (contents !== null) {
+            if (
+              contents.type === DIR &&
+              Object.keys(contents.contents).length > 0 &&
+              !args.recursive
+            ) {
+              this.api.printLine(`${this.toStringPath(path)} is not empty`);
+            } else {
+              this.addToFileSystem(path, undefined);
+            }
           }
         }
       },
@@ -248,17 +266,25 @@ export default class PseudoFileSystem {
             const contents = Object.values(dir.contents);
             this.api.printLine((
               <span>
-                {contents.map((item) => {
+                {contents.filter(item => typeof item !== 'undefined').map((item) => {
                   const styles = {
                     color: '#bdc3c7',
                     marginRight: 5,
                     width: 'calc(33% - 5px)',
+                    display: 'inline-block',
                   };
+                  if (contents.length > 3) {
+                    styles.marginBottom = 5;
+                  }
                   if (item.type === DIR) {
                     styles.color = '#2980b9';
                   }
                   return (
-                    <span styles={styles} key={`${item.name}-${item.type}`}>
+                    <span
+                      style={styles}
+                      title={item.type.toUpperCase()}
+                      key={`${item.name}-${item.type}`}
+                    >
                       {item.name}
                     </span>
                   );
